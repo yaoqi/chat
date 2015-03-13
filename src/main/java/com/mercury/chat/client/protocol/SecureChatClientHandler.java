ad84202e932952f78615fe78c13abc524e1717f8
yaoqi <yaoqis@gmail.com>
2015-03-14 01:30:12 +0800

    add message listeners attribute to channel.

--- src/main/java/com/mercury/chat/client/protocol/SecureChatClientHandler.java
+++ src/main/java/com/mercury/chat/client/protocol/SecureChatClientHandler.java
@@ -2,11 +2,16 @@ package com.mercury.chat.client.protocol;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.Attribute;
+
+import java.util.Collection;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.common.collect.Lists;
+import com.mercury.chat.common.constant.Constant;
 import com.mercury.chat.common.struct.protocol.Message;
 
 /**
@@ -17,8 +22,19 @@ public class SecureChatClientHandler extends SimpleChannelInboundHandler<Message
 	static final Logger logger = LogManager.getLogger(SecureChatClientHandler.class);
 	
     @Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    	Attribute<Collection<MessageListener>> listeners = ctx.channel().attr(Constant.listeners);
+    	listeners.setIfAbsent(Lists.newArrayList());
+	}
+
+	@Override
     public void messageReceived(ChannelHandlerContext ctx, Message msg) {
     	logger.log(Level.INFO, "Received Message:"+msg);
+    	Attribute<Collection<MessageListener>> attribute = ctx.channel().attr(Constant.listeners);
+    	Collection<MessageListener> listeners = attribute.get();
+		for (MessageListener listener : listeners) {
+			listener.onMessage(msg);
+		}
     }
 
     @Override
--- src/main/java/com/mercury/chat/common/constant/Constant.java
+++ src/main/java/com/mercury/chat/common/constant/Constant.java
@@ -2,12 +2,16 @@ package com.mercury.chat.common.constant;
 
 import io.netty.util.AttributeKey;
 
+import java.util.Collection;
+
+import com.mercury.chat.client.protocol.MessageListener;
 import com.mercury.chat.user.User;
 
 
 public interface Constant {
 	
 	public final static AttributeKey<User> userInfo = AttributeKey.valueOf("user");
+	public final static AttributeKey<Collection<MessageListener>> listeners = AttributeKey.valueOf("listeners");
 	
 }
 
