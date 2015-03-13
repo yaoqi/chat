10d75df6d83519193e79b59b88e748d9f0b2f2e4
yaoqi <yaoqis@gmail.com>
2015-03-14 01:51:02 +0800

    implement the add message listener function.

--- src/main/java/com/mercury/chat/client/protocol/impl/SessionImpl.java
+++ src/main/java/com/mercury/chat/client/protocol/impl/SessionImpl.java
@@ -1,13 +1,16 @@
 package com.mercury.chat.client.protocol.impl;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 import io.netty.channel.Channel;
+import io.netty.util.Attribute;
 
 import com.google.common.collect.Lists;
 import com.mercury.chat.client.protocol.MessageListener;
 import com.mercury.chat.client.protocol.Session;
+import com.mercury.chat.common.constant.Constant;
 import com.mercury.chat.common.struct.protocol.Message;
 
 public class SessionImpl implements Session {
@@ -42,7 +45,9 @@ public class SessionImpl implements Session {
 	}
 	
 	public void addMessageListener(MessageListener messageListener){
-		//TODO
+		Attribute<Collection<MessageListener>> attribute = channel.attr(Constant.listeners);
+    	Collection<MessageListener> listeners = attribute.get();
+    	listeners.add(messageListener);
 		return;
 	}
 
