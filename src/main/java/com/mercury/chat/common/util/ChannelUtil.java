07317fbc2449ddd478eec327c3727a6526caec32
yaoqi <yaoqis@gmail.com>
2015-03-14 02:11:32 +0800

    add util method to simple the code.

---
+++ src/main/java/com/mercury/chat/common/util/ChannelUtil.java
@@ -0,0 +1,20 @@
+package com.mercury.chat.common.util;
+
+import io.netty.channel.Channel;
+import io.netty.util.AttributeKey;
+
+public class ChannelUtil {
+	
+	public static <T> T getAttribute(Channel channel, AttributeKey<T> attributeKey){
+		return channel.attr(attributeKey).get();
+	}
+	
+	public static <T> void setAttribute(Channel channel, AttributeKey<T> attributeKey, T value){
+		channel.attr(attributeKey).setIfAbsent(value);
+	}
+	
+	public static <T> boolean hasAttribute(Channel channel, AttributeKey<T> attributeKey){
+		return channel.hasAttr(attributeKey);
+	}
+	
+}
