f9bbe4d64b42b5073ee5854c0f942aa9bc2b51a7
yaoqi <yaoqis@gmail.com>
2015-03-16 02:09:56 +0800

    change the client side code.

--- src/main/java/com/mercury/chat/client/protocol/SecureChatClient.java
+++ src/main/java/com/mercury/chat/client/protocol/SecureChatClient.java
@@ -1,5 +1,7 @@
 package com.mercury.chat.client.protocol;
 
+import java.net.SocketException;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
@@ -17,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 
 import com.mercury.chat.client.protocol.impl.ConnectionImpl;
 
-public final class SecureChatClient extends Thread{
+public final class SecureChatClient{
     
 	static final Logger logger = LogManager.getLogger(SecureChatClient.class);
 	
@@ -26,93 +28,30 @@ public final class SecureChatClient extends Thread{
 	//default value for client side.
 	static final String HOST = System.getProperty("host", "127.0.0.1");
     static final int PORT = Integer.parseInt(System.getProperty("port", "8992"));
-    private volatile Channel channel;
-    private String host;
-    private int port;
-    
-    private Object lock = new Object();
-    
-    public Channel getChannel(){
-    	synchronized(lock){
-    		while(channel==null){
-    			try {
-					lock.wait();
-				} catch (InterruptedException e) {
-					//ignore
-				}
-    		}
-    		return channel;
-    	}
-    }
-    
-    public SecureChatClient(String host, int port) {
-		super();
-		this.host = host;
-		this.port = port;
-		this.start();
-	}
-
-	@Override
-	public void run() {
-		
-		Runtime.getRuntime().addShutdownHook(new Thread(){
-
-			@Override
-			public void run() {
-				logger.info("cilent exit!");
-				try {
-					channel.closeFuture().sync();
-				} catch (InterruptedException e) {
-					logger.info(e);
-				}
-				group.shutdownGracefully();
-			}
-			
-		});
-		
-		try {
-			connectInner(host, port);
-		} catch (SSLException | InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-		
-	}
 
 	public static void main(String[] args) throws Exception {
         connect(HOST, PORT);
     }
 
     public static Connection connect(String host, int port) {
-		SecureChatClient secureChatClient = new SecureChatClient(host, port);
-		Connection connection = new ConnectionImpl().channel(secureChatClient.getChannel());
-		return connection;
+		return new ConnectionImpl().channel(new SecureChatClient().connectInner(host, port));
 	}
 
-	private void connectInner(String host, int port) throws SSLException,InterruptedException {
+	private Channel connectInner(String host, int port) {
 		// Configure SSL.
-        final SslContext sslCtx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
-
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group)
-             .option(ChannelOption.TCP_NODELAY, true)
-             .channel(NioSocketChannel.class)
-             .handler(new SecureChatClientInitializer(sslCtx));
-
-            // Start the connection attempt.
-            ChannelFuture sync = b.connect(host, port).sync();
-			Channel ch = sync.channel();
-            channel = ch;
-            synchronized(lock){
-    			lock.notify();
-        	}
-            ch.closeFuture().sync();
-        } catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-            // The connection is closed automatically on shutdown.
-            group.shutdownGracefully();
-        }
+        SslContext sslCtx;
+		try {
+			sslCtx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
+			Bootstrap b = new Bootstrap();
+	        b.group(group)
+	         .option(ChannelOption.TCP_NODELAY, true)
+	         .channel(NioSocketChannel.class)
+	         .handler(new SecureChatClientInitializer(sslCtx));
+	         return b.connect(host, port).sync().channel();
+		} catch (Exception e) {
+			group.shutdownGracefully();
+			throw new RuntimeException(e);
+		}
 	}
     
 }
