fa29e06c08fb8473948cb08d16e59a0d80fad72e
yaoqi <yaoqis@gmail.com>
2015-03-15 14:36:00 +0800

    inject the userMapper and messageMapper

--- src/main/java/com/mercury/chat/user/service/storer/db/UserServiceImpl.java
+++ src/main/java/com/mercury/chat/user/service/storer/db/UserServiceImpl.java
@@ -3,12 +3,27 @@ package com.mercury.chat.user.service.storer.db;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.mercury.chat.common.struct.IMessage;
+import com.mercury.chat.user.dao.MessageMapper;
+import com.mercury.chat.user.dao.UserMapper;
 import com.mercury.chat.user.entity.User;
 import com.mercury.chat.user.service.UserService;
 
+@Service
+@Transactional(propagation=Propagation.REQUIRED)
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Autowired
+	private MessageMapper messageMapper;
+	
 	@Override
 	public boolean login(String userId, String passWord) {
 		return false;
