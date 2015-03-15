29ab8b71fc094fd0164b39856b8b97c8e105e04b
yaoqi <yaoqis@gmail.com>
2015-03-15 13:15:44 +0800

    add chat exception.

---
+++ src/main/java/com/mercury/chat/common/exception/ChatException.java
@@ -0,0 +1,9 @@
+package com.mercury.chat.common.exception;
+
+public class ChatException extends RuntimeException {
+
+	private static final long serialVersionUID = -3020226599902573310L;
+	
+	
+
+}
