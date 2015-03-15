d841788a7df1386fd53de11700756e2e5b11be64
yaoqi <yaoqis@gmail.com>
2015-03-15 13:24:43 +0800

    add error code.

--- src/main/java/com/mercury/chat/common/exception/ChatException.java
+++ src/main/java/com/mercury/chat/common/exception/ChatException.java
@@ -4,6 +4,35 @@ public class ChatException extends RuntimeException {
 
 	private static final long serialVersionUID = -3020226599902573310L;
 	
+	private ErrorCode errorCode;
 	
+	public ChatException(ErrorCode messageCode) {
+		super();
+		this.errorCode = messageCode;
+	}
+	
+	public ChatException() {
+		super();
+	}
+
+	public ChatException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ChatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ChatException(String message) {
+		super(message);
+	}
+
+	public ChatException(Throwable cause) {
+		super(cause);
+	}
+	
+	public ErrorCode messageCode(){
+		return this.errorCode;
+	}
 
 }
---
+++ src/main/java/com/mercury/chat/common/exception/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.mercury.chat.common.exception;
+
+public enum ErrorCode {
+	
+}
