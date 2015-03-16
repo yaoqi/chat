2ac58191cfca0fa6469706404f29f6657f6e0305
yaoqi <yaoqis@gmail.com>
2015-03-17 00:09:57 +0800

    add default config

--- src/main/java/com/mercury/chat/common/TaskExecutor.java
+++ src/main/java/com/mercury/chat/common/TaskExecutor.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 
 public class TaskExecutor {
 	
-	public ExecutorService taskExecutor = Executors.newFixedThreadPool(50);
+	public ExecutorService taskExecutor = Executors.newFixedThreadPool(Integer.parseInt(System.getProperty("Task.Executor.Size", "50")));
 	
 	private static class SingletonHolder {
 		private static final TaskExecutor INSTANCE = new TaskExecutor();
