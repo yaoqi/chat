805ecc6f2a6d181ba87adc8592e7a146d45194fc
yaoqi <yaoqis@gmail.com>
2015-03-16 22:28:49 +0800

    add discard flag to message box.

--- src/main/java/com/mercury/chat/client/protocol/MessageBox.java
+++ src/main/java/com/mercury/chat/client/protocol/MessageBox.java
@@ -2,25 +2,43 @@ package com.mercury.chat.client.protocol;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import com.mercury.chat.common.struct.protocol.Message;
 
 public class MessageBox {
 
+	private volatile boolean discard = false;
+	
 	private BlockingQueue<Message> box = new LinkedBlockingQueue<Message>(1);
 	
 	public void put(Message msg){
-		box.offer(msg);
+		if(!discard){
+			box.offer(msg);
+		}
 	}
 	
 	public Message get(){
 		Message msg = null;
 		try {
-			msg = box.take();
+			msg = box.poll(10000, TimeUnit.MILLISECONDS);
 		} catch (InterruptedException e) {
 			//ignore
 		}
+		
+		if(msg == null){
+			setDiscard(true);
+		}
+		
 		return msg;
+	}
+
+	public boolean isDiscard() {
+		return discard;
+	}
+
+	public void setDiscard(boolean discard) {
+		this.discard = discard;
 	}
 	
 }
