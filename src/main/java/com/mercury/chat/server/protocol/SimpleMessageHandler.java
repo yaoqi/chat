b220d3d071f4af02f3816ee5ccde6b7239ec8a50
yaoqi <yaoqis@gmail.com>
2015-03-19 00:49:20 +0800

    XXX

---
+++ src/main/java/com/mercury/chat/server/protocol/SimpleMessageHandler.java
@@ -0,0 +1,29 @@
+package com.mercury.chat.server.protocol;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.mercury.chat.common.struct.protocol.Message;
+
+public abstract class SimpleMessageHandler extends SimpleChannelInboundHandler<Message> {
+
+	static final Logger logger = LogManager.getLogger(SimpleMessageHandler.class);
+	
+	@Override
+	protected void messageReceived(ChannelHandlerContext ctx, Message msg) throws Exception {
+		validate(ctx, msg);
+		logger.log(Level.INFO, "Received message : ---> "+ msg);
+		onMessage(ctx, msg);
+	}
+	
+	protected void validate(ChannelHandlerContext ctx, Message msg) {
+		
+	}
+
+	protected abstract void onMessage(ChannelHandlerContext ctx, Message msg) throws Exception;
+	
+}
