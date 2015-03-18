1e20a96763f27019ad993c62a8d5426589696133
yaoqi <yaoqis@gmail.com>
2015-03-18 21:15:36 +0800

    refactor the code change

--- src/main/java/com/mercury/chat/client/protocol/AbstractClientMessageHandler.java
+++ src/main/java/com/mercury/chat/client/protocol/AbstractClientMessageHandler.java
@@ -1,16 +1,45 @@
 package com.mercury.chat.client.protocol;
 
+import java.util.Collection;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import com.google.common.collect.Lists;
+import com.mercury.chat.common.MessageListener;
+import com.mercury.chat.common.MessageType;
 import com.mercury.chat.common.struct.protocol.Message;
 
-public class AbstractClientMessageHandler extends SimpleChannelInboundHandler<Message> {
+public abstract class AbstractClientMessageHandler extends SimpleChannelInboundHandler<Message> {
+	
+	static final Logger logger = LogManager.getLogger(AbstractClientMessageHandler.class);
+	
+	private MessageType messageType;
+	
+	private volatile Collection<MessageListener> listeners = Lists.newArrayList();
+	
+	public AbstractClientMessageHandler(MessageType messageType) {
+		super();
+		this.messageType = messageType;
+	}
+	
+	public void addMessageListener(MessageListener listener){
+		listeners.add(listener);
+	}
 
 	@Override
-	protected void messageReceived(ChannelHandlerContext ctx, Message msg) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    public void messageReceived(ChannelHandlerContext ctx, Message msg) {
+    	logger.log(Level.INFO, "Received Message:"+msg);
+		for (MessageListener listener : listeners) {
+			listener.onMessage(msg);
+		}
+		onMessage(ctx, msg);
+    }
+	
+    protected abstract void onMessage(ChannelHandlerContext ctx, Message msg);
 
 }
