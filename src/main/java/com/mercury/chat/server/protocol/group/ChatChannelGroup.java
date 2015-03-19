a1eac142b4a649555b860a4381780cf19255dec6
yaoqi <yaoqis@gmail.com>
2015-03-20 00:06:19 +0800

    XXX

--- src/main/java/com/mercury/chat/server/protocol/group/ChatChannelGroup.java
+++ src/main/java/com/mercury/chat/server/protocol/group/ChatChannelGroup.java
@@ -1,11 +1,49 @@
 package com.mercury.chat.server.protocol.group;
 
+import static com.mercury.chat.common.constant.Constant.userInfo;
+import static com.mercury.chat.common.util.Channels.get;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelId;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.EventExecutor;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+
 public class ChatChannelGroup extends DefaultChannelGroup {
 
+	private CacheLoader<String,Channel> cacheLoader = new CacheLoader<String,Channel>(){
+				@Override
+				public Channel load(String key) throws Exception {
+					return null;
+				}
+	};
+	
+	private RemovalListener<String,Channel> removalListener = new RemovalListener<String,Channel>(){
+
+		@Override
+		public void onRemoval(RemovalNotification<String, Channel> notification) {
+			//XXX
+		}
+		
+	};
+	
+	private LoadingCache<String,Channel> cache = CacheBuilder
+												.newBuilder()
+												.concurrencyLevel(4)
+												.initialCapacity(8)
+												.maximumSize(10000)
+												.removalListener(removalListener)
+												.recordStats()
+												.build(cacheLoader);
+	
+	public LoadingCache<String,Channel> getCache(){
+		return cache;
+	}
+	
 	public ChatChannelGroup(EventExecutor executor) {
 		super(executor);
 	}
@@ -16,12 +54,23 @@ public class ChatChannelGroup extends DefaultChannelGroup {
 
 	@Override
 	public boolean add(Channel channel) {
-		return super.add(channel);
+		boolean added = super.add(channel);
+		cache.put(get(channel,userInfo).getUserId(), channel);
+		return added;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		return super.remove(o);
+		Channel channel = null;
+		if (o instanceof ChannelId) {
+			ChannelId id = (ChannelId) o;
+			channel = find(id);
+		}else if(o instanceof Channel){
+			channel = (Channel) o;
+		}
+		cache.invalidate(get(channel ,userInfo).getUserId());
+		boolean removed = super.remove(o);
+		return removed;
 	}
 	
 	
--- src/main/java/com/mercury/chat/server/protocol/group/SessionManager.java
+++ src/main/java/com/mercury/chat/server/protocol/group/SessionManager.java
@@ -1,11 +1,10 @@
 package com.mercury.chat.server.protocol.group;
 
 import io.netty.channel.group.ChannelGroup;
-import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class SessionManager {
 	
-	public static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+	public static final ChannelGroup channels = new ChatChannelGroup(GlobalEventExecutor.INSTANCE);
 	
 }
