16174d087db308273870596efecf818306b89048
yaoqi <yaoqis@gmail.com>
2015-03-21 16:20:01 +0800

    add util method to check the response is ok or not.

--- src/main/java/com/mercury/chat/common/constant/StatusCode.java
+++ src/main/java/com/mercury/chat/common/constant/StatusCode.java
@@ -5,23 +5,42 @@ import org.apache.commons.lang.ObjectUtils;
 import com.mercury.chat.common.struct.IMessage;
 
 public enum StatusCode {
-	OK(200, "Operate Success"), 
+	OK(200, "Operate Success"){
+		@Override
+		public boolean isFailed() {
+			return false;
+		}
+	}, 
 	FAIL(400, "Operate Failure"),
 	UNAUTHORIZED(401, "Unauthorized"), 
 	INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
 	SERVICE_UNAVAILABLE(503, "Service Unavailable"),
 	NOT_LOGIN(600, "Invalid operation:not logged in"),
 	LOGGED_IN(601, "Invalid operation:already logged in"),
-	USER_LOGIN(700, "User Login"),
-	USER_LOGOFF(701, "User Logoff");
+	USER_LOGIN(700, "User Login"){
+		@Override
+		public boolean isFailed() {
+			return false;
+		}
+	},
+	USER_LOGOFF(701, "User Logoff"){
+		@Override
+		public boolean isFailed() {
+			return false;
+		}
+	};
 	
 	private int key;
 	
 	private String message;
-
+	
 	private StatusCode(int key, String message) {
 		this.key = key;
 		this.message = message;
+	}
+	
+	public boolean isFailed(){
+		return true;
 	}
 
 	public int key() {
