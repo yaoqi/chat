52f96efed68c1498f14bd0fa421c60074cd9e7b6
yaoqi <yaoqis@gmail.com>
2015-03-21 16:37:01 +0800

    XXX

--- src/main/java/com/mercury/chat/common/exception/ErrorCode.java
+++ src/main/java/com/mercury/chat/common/exception/ErrorCode.java
@@ -2,4 +2,23 @@ package com.mercury.chat.common.exception;
 
 public enum ErrorCode {
 	
+	CLOSED(1, "Connection Closed");
+	
+	private int key;
+	
+	private String message;
+	
+	private ErrorCode(int key, String message) {
+		this.key = key;
+		this.message = message;
+	}
+	
+	public int key() {
+		return key;
+	}
+
+	public String message() {
+		return message;
+	}
+	
 }
