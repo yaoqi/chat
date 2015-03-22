d70695b2dacc71d6c4aecaf2ac2db320210d992d
yaoqi <yaoqis@gmail.com>
2015-03-22 14:22:30 +0800

    add productSummaries to Order Summary.

--- src/main/java/com/mercury/chat/common/OrderSummary.java
+++ src/main/java/com/mercury/chat/common/OrderSummary.java
@@ -1,16 +1,34 @@
 package com.mercury.chat.common;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public class OrderSummary implements Serializable{
 	
 	private static final long serialVersionUID = 3958845095205198860L;
 	
-	public long orderId;
+	private long orderId;
+	
+	private Collection<ProductSummary> products;
 
-	public OrderSummary(long orderId) {
+	public OrderSummary() {
 		super();
+	}
+
+	public long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(long orderId) {
 		this.orderId = orderId;
+	}
+
+	public Collection<ProductSummary> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Collection<ProductSummary> products) {
+		this.products = products;
 	}
 	
 }
