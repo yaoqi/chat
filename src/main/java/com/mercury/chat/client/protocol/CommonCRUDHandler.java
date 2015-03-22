72be11c7e2a3d6dca61e0f03452be473679c1504
yaoqi <yaoqis@gmail.com>
2015-03-22 21:14:43 +0800

    XXX

--- src/main/java/com/mercury/chat/client/protocol/CommonCRUDHandler.java
+++ src/main/java/com/mercury/chat/client/protocol/CommonCRUDHandler.java
@@ -2,26 +2,31 @@ package com.mercury.chat.client.protocol;
 
 import static com.mercury.chat.common.MessageType.CRUD;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mercury.chat.common.MessageType;
 import com.mercury.chat.common.struct.protocol.Message;
 
-public class CommonCRUDHandler extends SimpleChannelInboundHandler<Message> {
+public class CommonCRUDHandler extends SimpleMessageHandler {
 
 	static final Logger logger = LogManager.getLogger(CommonCRUDHandler.class);
 	
 	@Override
-	 public void messageReceived(ChannelHandlerContext ctx, Message msg) throws Exception {
-		
-		if (CRUD.$(msg)) {
+	protected MessageType _() {
+		return CRUD;
+	}
+	
+	@Override
+	 public void messageReceived(ChannelHandlerContext ctx, Message msg) {
+		if (_().$(msg)) {
 			logger.log(Level.INFO, "Receive server CRUD response : ---> "+ msg);
 			
 		} else
 		    ctx.fireChannelRead(msg);
     }
+	
 	
 }
