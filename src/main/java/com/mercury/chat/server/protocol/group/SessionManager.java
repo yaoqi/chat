7f5a946c3f06cd2f68e94ab43647cec4b109e7f4
yaoqi <yaoqis@gmail.com>
2015-03-22 09:12:47 +0800

    XXX

--- src/main/java/com/mercury/chat/server/protocol/group/SessionManager.java
+++ src/main/java/com/mercury/chat/server/protocol/group/SessionManager.java
@@ -3,8 +3,27 @@ package com.mercury.chat.server.protocol.group;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mercury.chat.user.entity.User;
+
 public class SessionManager {
 	
 	public static final ChannelGroup channels = new ChatChannelGroup(GlobalEventExecutor.INSTANCE);
+	
+	public LoadingCache<String,User> uerCache = CacheBuilder
+			.newBuilder()
+			.concurrencyLevel(4)
+			.initialCapacity(8)
+			.maximumSize(10000)
+			.recordStats()
+			.build(new CacheLoader<String,User>(){
+				@Override
+				public User load(String key) throws Exception {
+					//TODO load the user to cache.
+					return null;
+				}
+			});
 	
 }
