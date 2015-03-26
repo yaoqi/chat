cf5f55cb3ec14c71d28c8f4ff5c9a5a1d72354b8
yaoqi <yaoqis@gmail.com>
2015-03-26 22:15:07 +0800

    remove unused codes.

--- src/main/java/com/mercury/chat/user/dao/OrderSummaryMapper.java
+++ src/main/java/com/mercury/chat/user/dao/OrderSummaryMapper.java
@@ -1,42 +1,17 @@
 package com.mercury.chat.user.dao;
 
-import com.mercury.chat.user.entity.OrderSummary;
-import com.mercury.chat.user.entity.OrderSummaryTemplate;
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.DeleteProvider;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.Param;
+
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.annotations.UpdateProvider;
 import org.apache.ibatis.type.JdbcType;
 
+import com.mercury.chat.user.entity.OrderSummary;
+import com.mercury.chat.user.entity.OrderSummaryTemplate;
+
 public interface OrderSummaryMapper {
-    @SelectProvider(type=OrderSummarySqlProvider.class, method="countByExample")
-    int countByExample(OrderSummaryTemplate example);
-
-    @DeleteProvider(type=OrderSummarySqlProvider.class, method="deleteByExample")
-    int deleteByExample(OrderSummaryTemplate example);
-
-    @Delete({
-        "delete from ORDER_SUMMARY",
-        "where ORDER_ID = #{orderId,jdbcType=BIGINT}"
-    })
-    int deleteByPrimaryKey(Long orderId);
-
-    @Insert({
-        "insert into ORDER_SUMMARY (ORDER_ID, SUMMARY)",
-        "values (#{orderId,jdbcType=BIGINT}, #{summary,jdbcType=VARCHAR})"
-    })
-    int insert(OrderSummary record);
-
-    @InsertProvider(type=OrderSummarySqlProvider.class, method="insertSelective")
-    int insertSelective(OrderSummary record);
 
     @SelectProvider(type=OrderSummarySqlProvider.class, method="selectByExample")
     @Results({
@@ -57,19 +32,4 @@ public interface OrderSummaryMapper {
     })
     OrderSummary selectByPrimaryKey(Long orderId);
 
-    @UpdateProvider(type=OrderSummarySqlProvider.class, method="updateByExampleSelective")
-    int updateByExampleSelective(@Param("record") OrderSummary record, @Param("example") OrderSummaryTemplate example);
-
-    @UpdateProvider(type=OrderSummarySqlProvider.class, method="updateByExample")
-    int updateByExample(@Param("record") OrderSummary record, @Param("example") OrderSummaryTemplate example);
-
-    @UpdateProvider(type=OrderSummarySqlProvider.class, method="updateByPrimaryKeySelective")
-    int updateByPrimaryKeySelective(OrderSummary record);
-
-    @Update({
-        "update ORDER_SUMMARY",
-        "set SUMMARY = #{summary,jdbcType=VARCHAR}",
-        "where ORDER_ID = #{orderId,jdbcType=BIGINT}"
-    })
-    int updateByPrimaryKey(OrderSummary record);
 }
--- src/main/java/com/mercury/chat/user/dao/ProductSummaryMapper.java
+++ src/main/java/com/mercury/chat/user/dao/ProductSummaryMapper.java
@@ -1,46 +1,19 @@
 package com.mercury.chat.user.dao;
 
-import com.mercury.chat.user.entity.ProductSummary;
-import com.mercury.chat.user.entity.ProductSummaryTemplate;
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.DeleteProvider;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.Param;
+
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.annotations.UpdateProvider;
 import org.apache.ibatis.type.JdbcType;
 
+import com.mercury.chat.user.entity.ProductSummary;
+import com.mercury.chat.user.entity.ProductSummaryTemplate;
+
 public interface ProductSummaryMapper {
-    @SelectProvider(type=ProductSummarySqlProvider.class, method="countByExample")
-    int countByExample(ProductSummaryTemplate example);
-
-    @DeleteProvider(type=ProductSummarySqlProvider.class, method="deleteByExample")
-    int deleteByExample(ProductSummaryTemplate example);
-
-    @Delete({
-        "delete from PRODUCT_SUMMARY",
-        "where PRODUCT_ID = #{productId,jdbcType=BIGINT}"
-    })
-    int deleteByPrimaryKey(Long productId);
-
-    @Insert({
-        "insert into PRODUCT_SUMMARY (PRODUCT_ID, PRODUCT_CODE, ",
-        "SUMMARY)",
-        "values (#{productId,jdbcType=BIGINT}, #{productCode,jdbcType=VARCHAR}, ",
-        "#{summary,jdbcType=VARCHAR})"
-    })
-    int insert(ProductSummary record);
-
-    @InsertProvider(type=ProductSummarySqlProvider.class, method="insertSelective")
-    int insertSelective(ProductSummary record);
-
-    @SelectProvider(type=ProductSummarySqlProvider.class, method="selectByExample")
+   
+	@SelectProvider(type=ProductSummarySqlProvider.class, method="selectByExample")
     @Results({
         @Result(column="PRODUCT_ID", property="productId", jdbcType=JdbcType.BIGINT, id=true),
         @Result(column="PRODUCT_CODE", property="productCode", jdbcType=JdbcType.VARCHAR),
@@ -61,20 +34,4 @@ public interface ProductSummaryMapper {
     })
     ProductSummary selectByPrimaryKey(Long productId);
 
-    @UpdateProvider(type=ProductSummarySqlProvider.class, method="updateByExampleSelective")
-    int updateByExampleSelective(@Param("record") ProductSummary record, @Param("example") ProductSummaryTemplate example);
-
-    @UpdateProvider(type=ProductSummarySqlProvider.class, method="updateByExample")
-    int updateByExample(@Param("record") ProductSummary record, @Param("example") ProductSummaryTemplate example);
-
-    @UpdateProvider(type=ProductSummarySqlProvider.class, method="updateByPrimaryKeySelective")
-    int updateByPrimaryKeySelective(ProductSummary record);
-
-    @Update({
-        "update PRODUCT_SUMMARY",
-        "set PRODUCT_CODE = #{productCode,jdbcType=VARCHAR},",
-          "SUMMARY = #{summary,jdbcType=VARCHAR}",
-        "where PRODUCT_ID = #{productId,jdbcType=BIGINT}"
-    })
-    int updateByPrimaryKey(ProductSummary record);
 }
